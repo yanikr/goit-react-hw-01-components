f2848e88bbbe99c9d4081f01fbe4fab448dbe37b
Yanik Rikhter <rihter.yan@gmail.com>
2022-10-10 23:29:08 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -19,8 +19,10 @@ export const App = () => {
         stats={user.stats} />
       
       <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
 
       <FriendList friends={friends} />
+      
       <TransactionHistory items={transactions} />
     </div>
   );
